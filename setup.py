5b8c7758db66b4b20702908e133efd70675415a8
Matthew Bailey <31496100+MattLearn@users.noreply.github.com>
2021-05-28 10:25:05 -0400

    output adjustment

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.netxcalculator',
-      version='1.0.1',
+      version='1.0.2',
       description=('a simple calculator for godigital costs and discounts'),
       long_description='# docassemble.netxcalculator\r\n\r\na simple calculator for godigital costs and discounts\r\n\r\n## Author\r\n\r\nMatthew Bailey, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
